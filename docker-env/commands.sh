6d69613ef735509250a6b4f6f6caf62d81f0a02d
Slawek Ludwiczak <slawek.ludwiczak@gmail.com>
2024-04-17 14:14:59 +0200

    env file

--- docker-env/commands.sh
+++ docker-env/commands.sh
@@ -1,4 +1,4 @@
 docker build -t javastart/docker-application:latest .
 docker run --rm --e LANG=pl --name docker-app javastart/docker-application
-docker run --rm --env-file=polish.env --name docker-app javastart/docker-application
-docker run --rm -e LANG=es --env-file=polish.env --name docker-app javastart/docker-application
+docker run --rm --env-file=.env --name docker-app javastart/docker-application
+docker run --rm -e LANG=es --env-file=.env --name docker-app javastart/docker-application
