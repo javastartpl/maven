32cc90eb8f185d5e9a56ade35b84179c599f8bda
Sławek Ludwiczak <slawek.ludwiczak@gmail.com>
2024-04-22 11:55:25 +0200

    volume changes

--- out/production/maven/commands.sh
+++
@@ -1,3 +0,0 @@
-docker build -t javastart/docker-application:latest .
-docker volume create docker-app-volume
-docker run -it --name docker-app-v2 -v docker-app-volume:/opt/volume-data javastart/docker-application
