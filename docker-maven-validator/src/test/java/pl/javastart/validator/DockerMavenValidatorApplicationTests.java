ec9063b4b8d3857f60c2e22cd62d299097a07988
Slawek Ludwiczak <slawek.ludwiczak@gmail.com>
2024-05-07 12:52:36 +0200

    docker + maven wip

---
+++ docker-maven-validator/src/test/java/pl/javastart/validator/DockerMavenValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerMavenValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
